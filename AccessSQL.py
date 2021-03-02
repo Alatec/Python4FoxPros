2f8df00280b8591d1675246c102e8945ebb870d1
Andrew <andrew.johnson44@student.montana.edu>
2021-03-01 17:26:13 -0700

    ODBC Connection string is working

--- AccessSQL.py
+++ AccessSQL.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import pyodbc
 
-# Can't verify but I assmue this works
-# con_string = 'DRIVER={SQL Server};SERVER='+ <server> +';DATABASE=' + <database>
-# cnxn = pyodbc.connect(con_string)
-# query = """
-#   SELECT <field1>, <field2>, <field3>
-#   FROM result
-# """
-# result_port_map = pd.read_sql(query, cnxn)
-# result_port_map.columns.tolist()
+# May not be required for Windows, but had to install the correct odbc driver for linux
+# https://docs.microsoft.com/en-us/sql/connect/odbc/microsoft-odbc-driver-for-sql-server?view=sql-server-ver15
 
-# For testing, I made some dummy data
-customer_sales = pd.read_csv('DataGenTools/SalesTable.csv')
+con_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=Desktop-1;DATABASE=RNU; UID=Andrew; PWD=Andrew;'
+conn = pyodbc.connect(con_string)
+query = """
+  SELECT TOP 100 * FROM Customers;
+"""
+customers = pd.read_sql(query, conn)
 
-customers = customer_sales.groupby('customer_id')
+
+
+# I don't know what you'd use this for, but here's a cool groupby example
+customers_by_type = customers.groupby('CUSTOMERTYPE')
 
 # Iterate over every customer. 'Customer' is just a table
 for customer in customers:
